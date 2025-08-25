368349d8a3eed842b5af438024822db55f9856a6
RHYTHM SAHA <rs.2001.saha@gmail.com>
2025-08-25 20:30:54 +0530

    feat: add DashboardSidebar component and integrate it into HomeLayout

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,7 +1,7 @@
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/headers/Navbar";
-import clsx from "clsx";
-import { FolderKanban, LayoutDashboard, User } from "lucide-react";
+
+import DashboardSidebar from "../components/sidebar/DashboardSidebar";
 
 const HomeLayout = ({ children }) => {
   return (
@@ -9,7 +9,7 @@ const HomeLayout = ({ children }) => {
       <Navbar />
 
       <div className="flex h-[calc(100vh-4rem)]">
-        <div className="hidden lg:block lg:w-[200px] xl:w-[240px] 2xl:w-xs border-r border-gray-50 h-full">
+        {/* <div className="hidden lg:block lg:w-[200px] xl:w-[240px] 2xl:w-xs border-r border-gray-50 h-full">
           <nav className="mt-4 mx-auto w-11/12">
             <div className="flex flex-col items-stretch gap-1">
               <NavLink
@@ -55,7 +55,9 @@ const HomeLayout = ({ children }) => {
               </NavLink>
             </div>
           </nav>
-        </div>
+        </div> */}
+
+        <DashboardSidebar />
 
         <div className="flex-1">{children ? children : <Outlet />}</div>
       </div>
---
+++ src/components/sidebar/DashboardSidebar.jsx
@@ -0,0 +1,57 @@
+import clsx from "clsx";
+import { FolderKanban, LayoutDashboard, User } from "lucide-react";
+import { NavLink } from "react-router-dom";
+
+const DashboardSidebar = () => {
+  return (
+    <div className="hidden lg:block lg:w-[200px] xl:w-[240px] 2xl:w-xs border-r border-gray-50 h-full">
+      <nav className="mt-4 mx-auto w-11/12">
+        <div className="flex flex-col items-stretch gap-1">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              clsx(
+                isActive ? "text-blue-600 bg-blue-100" : "text-gray-600 hover:bg-gray-100",
+                "px-6 py-3  text-sm  w-full rounded-lg font-medium",
+                "flex gap-2 items-center justify-start"
+              )
+            }
+          >
+            <LayoutDashboard />
+            <span>Dashboard</span>
+          </NavLink>
+
+          <NavLink
+            to="/projects"
+            className={({ isActive }) =>
+              clsx(
+                isActive ? "text-blue-600 bg-blue-100" : "text-gray-600 hover:bg-gray-100",
+                "px-6 py-3  text-sm  w-full rounded-lg font-medium",
+                "flex gap-2 items-center justify-start"
+              )
+            }
+          >
+            <FolderKanban />
+            <span>Projects</span>
+          </NavLink>
+
+          <NavLink
+            to="/my-account"
+            className={({ isActive }) =>
+              clsx(
+                isActive ? "text-blue-600 bg-blue-100" : "text-gray-600 hover:bg-gray-100",
+                "px-6 py-3  text-sm  w-full rounded-lg font-medium",
+                "flex gap-2 items-center justify-start"
+              )
+            }
+          >
+            <User />
+            <span>Profile</span>
+          </NavLink>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default DashboardSidebar;
