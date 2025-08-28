2b90230a04a5c49e613854eaa8374fad75fed7e1
Suraj00345 <surajkumarmanna2000@gmail.com>
2025-08-28 11:56:09 +0530

    adding Go back Button

--- src/components/Tasks/BoardHeader.jsx
+++ src/components/Tasks/BoardHeader.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
 
-const BoardHeader = ({Name}) => {
+const BoardHeader = ({ Name }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="mb-8 flex-shrink-0 ">
+    <div className="mb-8 flex-shrink-0 flex justify-between items-center">
       <h1 className="text-2xl font-bold text-black mb-2">{Name}</h1>
+      <button
+        onClick={() => navigate("/projects")}
+        className="h-10 flex items-center gap-2 p-2 text-white w-auto border bg-blue-600 rounded-lg hover:cursor-pointer"
+      >
+        <ArrowLeft size={13} /> Go Back
+      </button>
     </div>
   );
 };
+
 export default BoardHeader;
